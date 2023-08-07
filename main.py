0abba2f648aa3dc6a9cb4153cac3693c71e0863f
german <german.ampuero@unmsm.edu.pe>
2023-08-07 01:10:24 -0500

    ya está el back falta terminar el front

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from db.base import Base
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from db.session import engine
 from apis.base import api_router
@@ -19,11 +20,23 @@ def mount_static_folder(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def add_cors(app):
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["GET"],
+        allow_headers=["*"],
+    )
+
+
 def start_application():
     app = FastAPI()
     create_tables()
     include_router(app)
     mount_static_folder(app)
+    add_cors(app)
     return app
 
 
